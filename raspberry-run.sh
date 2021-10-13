e6aced6964792ed1ee35daa319104189973e11f4
Patrick Beaumont <p.beaumont@hatsoffsoftware.co.uk>
2021-10-13 13:35:31 +0100

    updated raspberry pi setup

---
+++ raspberry-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo node-red-pi --max-old-space-size=256
--- raspberry-setup.sh
+++ raspberry-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get install nodered -y
-sudo -- sh -c 'cd /root/.node-red && npm i modbus-serial'
+sudo -- sh -c 'cd /root/.node-red && npm i modbus-serial && sed 's/^\s*functionGlobalContext: {/    functionGlobalContext: {\n        modbusserial:require('modbus-serial'),/' /root/.node-red/settings.js'
